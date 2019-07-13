575945a8046aa194759215ad7601b3e2985929d9
kuboshoji <jiangzhijiubao@gmail.com>
2019-07-13 12:12:01 +0900

    ログイン処理の実装

--- app.py
+++ app.py
@@ -1,2 +1,16 @@
+from db import get_user
+from users import User
+
+
 if __name__ == "__main__":
-    pass
+
+    while True:
+        name = input("ユーザー名を入力してください。: ")
+        
+        user_ = get_user(name)
+        if user_:
+            user = User(user_["id"], user_["name"], user_["age"])
+            break
+    
+    print(user.name + "でログインしました。")
+            
